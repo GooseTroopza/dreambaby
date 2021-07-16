ab70d85e74d7ea10e8cfa63ee947c6d0d55679e6
GooseTroopza <70577566+GooseTroopza@users.noreply.github.com>
2021-07-16 11:26:19 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,9 @@
-print("Hello")
+import touchphat
+from pygame import mixer
+
+
+@touchphat.on_touch("1")
+def start():
+    mixer.init()
+    mixer.music.load('/home/pi/baby_goes_what_home/server/public/files/dream.mp3')
+    mixer.music.play()
